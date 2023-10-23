4c6c30fd3bbc89bd41653eebd0d7156630db187d
Elliana May <me@mause.me>
2023-10-23 11:59:22 +0800

    add some checking of js files

--- test/worker.js
+++ test/worker.js
@@ -2,6 +2,10 @@ const { parentPort, workerData } = require('worker_threads');
 
 const duckdb = require('..'); // The presence of this line alone causes the issue
 
+if (!parentPort) {
+	throw new Error('This script must be run as a worker thread.');
+}
+
 new duckdb.Database(":memory:").all("SELECT 42 AS answer", function (err, res) {
     parentPort.postMessage(JSON.stringify({workerData, pid: process.pid, err, res}));
 });
